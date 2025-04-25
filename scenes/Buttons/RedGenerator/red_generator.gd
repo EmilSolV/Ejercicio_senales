053617a6ca4774bf3e5c71ad48156174d9184f50
Emil Vasquez <102248960+EmilSolV@users.noreply.github.com>
2025-04-25 02:18:39 -0300

    Instanciar BlockRed y BlockViolet dinamicamente

--- scenes/Buttons/RedGenerator/red_generator.gd
+++ scenes/Buttons/RedGenerator/red_generator.gd
@@ -1,5 +1,10 @@
 extends TextureButton
 
+@onready var BlockRed = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
-func _on_button_down() -> void:
-	pass # Replace with function body.
+func _on_button_down():
+	generate_block()
+
+func generate_block():
+	var new_block = BlockRed.instantiate()
+	get_tree().current_scene.add_child(new_block)
--- scenes/Buttons/VioletGenerator/violet_generator.gd
+++ scenes/Buttons/VioletGenerator/violet_generator.gd
@@ -1,5 +1,10 @@
 extends TextureButton
 
+@onready var BlockViolet = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
 
-func _on_button_down() -> void:
-	pass # Replace with function body.
+func _on_button_down():
+	generate_block()
+
+func generate_block():
+	var new_block = BlockViolet.instantiate()
+	get_tree().current_scene.add_child(new_block)
